7ff4fed3246528355a7feeeb31a60864e0c611d2
Jacob Hampton <jhampton792@gmail.com>
2021-03-25 16:15:33 -0500

    Added User model with base level validation

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, uniqueness: { case_sensitive: false }
+    validates :password, length: { minimum: 8, maximum: 40 }
+    
+end
