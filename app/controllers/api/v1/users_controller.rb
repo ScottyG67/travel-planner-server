5445ae0ba85b883ec4b27d8fe8b178f5e08ee4c8
Jacob Hampton <jhampton792@gmail.com>
2021-03-26 08:39:04 -0500

    Added routes and controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+    
+    def create
+        user = User.create(user_params)
+        
+        if user.valid?
+            render json: user, only: [:first_name, :last_name, :username], status: :created
+        else
+            render json: {error: 'failed to create user'}, status: :not_acceptable
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :password)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
 
 end
