6125cfe765084f065f2d5fc3566929f3c45f3b18
Jacob Hampton <jhampton792@gmail.com>
2021-03-26 11:20:55 -0500

    Create new user and produce upon creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
     
     def create
-        user = User.create(user_params)
+        @user = User.create(user_params)
         
-        if user.valid?
-            render json: user, only: [:first_name, :last_name, :username], status: :created
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            # render json: user, only: [:first_name, :last_name, :username], status: :created
+            render json: { user: @user.to_json(only: [:first_name, :last_name, :username]), jwt: @token}, status: :created
         else
             render json: {error: 'failed to create user'}, status: :not_acceptable
         end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,41 @@
 class ApplicationController < ActionController::API
+
+    before_action :authorized
+    
     def encode_token(payload)
-        JWT.encode(payload, AUTH_SECRET_KEY)
+        JWT.encode(payload, ENV['AUTH_SECRET_KEY'])
     end
+
+    def auth_header
+        #looking for 'Authorization': 'Bearer <token>' in headers: {...}
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            ## ['Bearer', '<token>']
+
+            begin
+                JWT.decode(token, ENV['AUTH_SECRET_KEY'], true, algorithm: 'HS256')[0]
+                ##JWT.decode => [payload]
+            rescue JWT::DecodeError
+                nil
+            end
+
+        end
+
+    end
+
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
 end
